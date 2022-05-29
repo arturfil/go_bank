40046a115a90beaaeed21696a7aee29ae1635c2d
arturfil <arturfil@hotmail.com>
2022-05-29 15:44:07 -0400

    added config and env variables

--- main.go
+++ main.go
@@ -3,20 +3,19 @@ package main
 import (
 	"bank/api"
 	db "bank/db/sqlc"
+	"bank/util"
 	"database/sql"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver     = "postgres"
-	dbSource     = "postgresql://root:secret@localhost:5432/bank?sslmode=disable"
-	serverAdress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Couldn't load configurations", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAdress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server", err)
 	}
